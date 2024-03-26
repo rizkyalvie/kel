6468714b7dcf9ac6c34ff19610c08a8bd1e4a995
rizkyalvie <rizkyalviee@gmail.com>
2024-03-26 10:13:33 +0700

    Text fixes

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
       <div
             className="max-w-[36rem] text-center flex flex-col justify-center items-center gap-y-5 "
         >
-            <h1 className={`${bigShoulders.className} text-6xl`}>Kel's Epic Library</h1>
+            <h1 className={`${bigShoulders.className} text-6xl`}>{`Kel's Epic Library`}</h1>
             <p className={`${junge.className} text-lg`}>Step into the treasure trove of my favorite reads, a sanctuary where books reveal their magic through my curated recommendations!</p>
         </div>
     </header>
@@ -26,7 +26,7 @@ export function HeaderDetail() {
         href="/"
         >
           
-      <h1 className={`${bigShoulders.className} text-3xl`}>Kel's Epic Library</h1>
+      <h1 className={`${bigShoulders.className} text-3xl`}>{`Kel's Epic Library`}</h1>
         </Link>
       </div>
     </header>
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ function Intro() {
         <div
             className="max-w-[24rem] text-center flex flex-col gap-y-3"
         >
-            <p className={`${bigShoulders.className} text-6xl`}>Kel's Epic Library</p>
+            <p className={`${bigShoulders.className} text-6xl`}>{`Kel's Epic Library`}</p>
             <p>Step into the treasure trove of my favorite reads, a sanctuary where books reveal their magic through my curated recommendations!</p>
         </div>
     </section>
